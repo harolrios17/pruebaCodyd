4337d8a7a2e44437d2691c1fbe0e2d8fc591b3f7
Harol <harol.rios17@gmail.com>
2023-07-28 23:30:56 -0500

    Cambios modelo vote

--- prueba_codyd/models/vote.py
+++ prueba_codyd/models/vote.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class Vote(models.Model):
     _name = 'vote'
@@ -18,3 +19,19 @@ class Vote(models.Model):
                 record.unique_vote = existing_votes == 0
             else:
                 record.unique_vote = False
+
+    @api.model
+    def create(self, vals):
+        voting_process_id = vals.get('voting_process_id')
+        if voting_process_id:
+            voting_process = self.env['voting.process'].browse(voting_process_id)
+            if voting_process.state != 'in_process':
+                raise ValidationError('La votación no está en "Proceso". No se puede votar.')
+
+            voter_id = vals.get('voter')
+            if voter_id:
+                existing_vote = self.search([('voter', '=', voter_id), ('voting_process_id', '=', voting_process_id)])
+                if existing_vote:
+                    raise ValidationError('Ya se registró un voto para este sufragante.')
+
+        return super(Vote, self).create(vals)
